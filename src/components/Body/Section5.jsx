6495d5983c639cde447b122031dfb145a6eddb48
thiagooo14 <thiagooo15@gmail.com>
2021-09-05 14:31:37 -0300

    body all done

--- src/components/Body/Section5.jsx
+++ src/components/Body/Section5.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { diary } from '../../assets/images';
+import './scss/Section5.scss';
 
 function Section5() {
   return (
-    <div className='section-text'>
-      <img src={diary} alt='diary' className='diary' />
-      <h2>Coming soon: Nanny Share Daily Diary!</h2>
-      <p>
-        With the Hapu daily diary your nanny will be able to update you and your
-        sharers with photos and commentary of the day. You and sharers will
-        receive notifications and you’ll be able to login to view the daily
-        adventures fo your little ones. We can’t wait!
-      </p>
+    <div >
+      <section className='section5'>
+        <img src={diary} alt='diary' className='diary' />
+        <div className='section-text'>
+          <h1>Coming soon: Nanny Share Daily Diary!</h1>
+          <p>
+            With the Hapu daily diary your nanny will be able to update you and
+            your sharers with photos and commentary of the day. You and sharers
+            will receive notifications and you’ll be able to login to view the
+            daily adventures fo your little ones. We can’t wait!
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
