74b0dd0cba279ced0f8a37d95e443ed6a24708d2
thiagooo14 <thiagooo15@gmail.com>
2021-09-05 14:10:01 -0300

    body section 3 done

--- src/components/Body/Section3.jsx
+++ src/components/Body/Section3.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { price } from '../../assets/images';
+import './scss/Section3.scss';
 
 function Section3() {
   return (
-    <div className='section-text'>
-      <img src={price} alt='payment exemple' />
-      <h2>Shared payments made simple</h2>
-      <p>
-        Sometimes it’s hard enough just sharing a restaurant bill. Try sharing
-        that bill week in, week out and you might encounter more than a few
-        snares. But not with Hapu. Simply set your rates and our automated
-        payment system takes care of the rest. You need never talk money or who
-        owes what.
-      </p>
-      <a href='/'>Read how Bridget’s share (without Hapu) ended over $15</a>
+    <div>
+      <section className='section3'>
+        <img src={price} alt='payment exemple' />
+        <div className='section-text'>
+          <h1>Shared payments made simple</h1>
+          <p>
+            Sometimes it’s hard enough just sharing a restaurant bill. Try
+            sharing that bill week in, week out and you might encounter more
+            than a few snares. But not with Hapu. Simply set your rates and our
+            automated payment system takes care of the rest. You need never talk
+            money or who owes what.
+          </p>
+          <a href='/'>Read how Bridget’s share (without Hapu) ended over $15</a>
+        </div>
+      </section>
+
+      <div className='divider' />
     </div>
   );
 }
